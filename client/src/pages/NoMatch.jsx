b5e1a6c635750aaf47155fcea660c9064dc6cb23
talbdbd <bendavid.bd.tal@gmail.com>
2024-07-15 19:25:09 -0700

    added E-coffee logo, implemented to Nav Bar, small fix with nomatch

--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -6,7 +6,7 @@ const NoMatch = () => {
       <Jumbotron>
         <h1>404 Page Not Found</h1>
         <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
+          <span role="img" aria-label="Coffee Emoji">
             ☕
           </span>
         </h1>
