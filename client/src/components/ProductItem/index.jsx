0293acdbd8a70df3e29219bb63b65c269272600d
Alfred Wallace <contact@alfredwallace.me>
2024-07-15 20:33:40 -0700

    Added product images

--- client/src/components/ProductItem/index.jsx
+++ client/src/components/ProductItem/index.jsx
@@ -1,8 +1,51 @@
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
-import { useStoreContext } from '../../utils/GlobalState';
+import { useStoreContext } from '../../utils/GlobalState'
+import breakfastBlend from '../../assets/breakfastBlend.png'
+import coffeeBeans from '../../assets/coffeeBeans.png';
+import coffeeSample from '../../assets/coffeeSample.png'
+import coffeeStix from '../../assets/coffeeStix.png'
+import decafPod from '../../assets/decafPod.png'
+import espressoBlend from '../../assets/espressoBlend.png'
+import espressoPods from '../../assets/espressoPods.png'
+import espressoStix from '../../assets/espressoStix.png'
+import hazelnutPod from '../../assets/hazelnutPod.png'
+import hojicha from '../../assets/hojicha.png'
+import hojichaLatte from '../../assets/hojichaLatte.png'
+import hojichaStix from '../../assets/hojichaStix.png'
+import matcha from '../../assets/matcha.png'
+import matchaStix from '../../assets/matchaStix.png'
+import matchaLatte from '../../assets/matchaLatte.png'
+import mugs from '../../assets/mugs.png'
+import grinder from '../../assets/grinder.png'
+import tumbler from '../../assets/tumbler.png'
+import vanillaPod from '../../assets/vanillaPod.png'
 
-function ProductItem({image, name, _id, quantity, price}) {
+const imageMap = {
+  breakfastBlend,
+  coffeeBeans,
+  coffeeSample,
+  coffeeStix,
+  decafPod,
+  espressoBlend,
+  espressoPods,
+  espressoStix,
+  matcha,
+  matchaLatte,
+  matchaStix,
+  hojicha,
+  hojichaLatte,
+  hojichaStix,
+  hazelnutPod,
+  mugs,
+  grinder,
+  tumbler,
+  vanillaPod
+}
+
+
+
+function ProductItem({ image, name, _id, quantity, price }) {
   const item = {
     image,
     name,
@@ -21,7 +64,8 @@ function ProductItem({image, name, _id, quantity, price}) {
       <Link to={`/products/${_id}`}>
         <img
           alt={name}
-          src={`/images/${image}`}
+          src={imageMap[image]}
+          style={{ height:'100px', width:'100px'}}
         />
         <p>{name}</p>
       </Link>
@@ -30,8 +74,8 @@ function ProductItem({image, name, _id, quantity, price}) {
         <span>${price}</span>
       </div>
       <button onClick={() => {
-            handleClick(item);
-          }}
+        handleClick(item);
+      }}
       >Add to cart</button>
     </div>
   );
